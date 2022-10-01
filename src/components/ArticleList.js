03f553f4d5ac9ee38ece7e25de0a78eca4a816ba
steve <stevemighty2030@gmail.com>
2022-10-01 17:34:44 +0300

    the lab is complete

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Article from '../components/Article';
+
+function ArticleList({posts}) {
+  const articles = posts.map(post=> {
+    return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
+});
+  return (
+    <main>
+      {articles}
+    </main>
+
+  )
+}
+
+export default ArticleList
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Header({name}) {
+  return (
+    <h1>{name}</h1>
+  )
+}
+export default Header
